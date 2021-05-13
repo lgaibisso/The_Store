4f2a442d9e79d99fb21294f8355ebdb157506395
Leonardo Gaibisso <leonardo.gaibisso@infocorp.com.uy>
2021-05-13 06:47:27 -0300

    Add project files.

---
+++ The_Store/Models/ApplicationUser.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace The_Store.Models
+{
+    public class ApplicationUser : IdentityUser
+    {
+    }
+}
